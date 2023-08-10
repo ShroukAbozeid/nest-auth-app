73a9f3733ad8ad4d075176c3ccb1aad6ba1ff66d
ShroukAbozeid <shroookabozaid@gmail.com>
2023-08-10 18:26:20 +0300

    adding tyeorm and passport libraries and basic simple username and password auth

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Render, UseGuards, Request } from '@nestjs/common';
 import { AppService } from './app.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('index')
+  root(@Request() req) {
+    return { message: 'Hello Shrouq' };
   }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    console.log(req)
+    return req.user;
+  }
+  
 }
